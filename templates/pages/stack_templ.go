fb41d2cf26a8906dbda7317904343b7136fd7199
BarryPRE_78 <barryprendergast78@gmail.com>
2026-05-13 16:33:45 +0200

    Regenerate stack page

--- templates/pages/stack_templ.go
+++ templates/pages/stack_templ.go
@@ -268,7 +268,7 @@ fly deploy`)
 			}
 			return nil
 		})
-		templ_7745c5c3_Err = layout.Base("Stack", csrfToken).Render(templ.WithChildren(ctx, templ_7745c5c3_Var2), templ_7745c5c3_Buffer)
+		templ_7745c5c3_Err = layout.Base("Stack", csrfToken, "/stack").Render(templ.WithChildren(ctx, templ_7745c5c3_Var2), templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
