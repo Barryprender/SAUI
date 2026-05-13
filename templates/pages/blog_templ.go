76d37b7d9ed8f11202352838af08434d409ee373
BarryPRE_78 <barryprendergast78@gmail.com>
2026-05-13 16:33:45 +0200

    Regenerate blog page

--- templates/pages/blog_templ.go
+++ templates/pages/blog_templ.go
@@ -49,7 +49,7 @@ func Blog(csrfToken string) templ.Component {
 			}
 			return nil
 		})
-		templ_7745c5c3_Err = layout.Base("Blog", csrfToken).Render(templ.WithChildren(ctx, templ_7745c5c3_Var2), templ_7745c5c3_Buffer)
+		templ_7745c5c3_Err = layout.Base("Blog", csrfToken, "/blog").Render(templ.WithChildren(ctx, templ_7745c5c3_Var2), templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
