246084c4193f29b2fe6993d4e4dca2da27c8f74c
BarryPRE_78 <barryprendergast78@gmail.com>
2026-05-13 16:34:02 +0200

    Regenerate cases_healthcare page

--- templates/pages/cases_healthcare_templ.go
+++ templates/pages/cases_healthcare_templ.go
@@ -231,7 +231,7 @@ func CaseHealthcare(csrfToken string) templ.Component {
 			}
 			return nil
 		})
-		templ_7745c5c3_Err = layout.Base("Case Study: Healthcare Booking", csrfToken).Render(templ.WithChildren(ctx, templ_7745c5c3_Var2), templ_7745c5c3_Buffer)
+		templ_7745c5c3_Err = layout.Base("Case Study: Healthcare Booking", csrfToken, "/cases").Render(templ.WithChildren(ctx, templ_7745c5c3_Var2), templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
