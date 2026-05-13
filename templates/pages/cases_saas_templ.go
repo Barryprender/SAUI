0f96743fd3c79519c874c220daf0b78bec772235
BarryPRE_78 <barryprendergast78@gmail.com>
2026-05-13 16:34:02 +0200

    Regenerate cases_saas page

--- templates/pages/cases_saas_templ.go
+++ templates/pages/cases_saas_templ.go
@@ -309,7 +309,7 @@ func CaseSaaS(csrfToken string) templ.Component {
 			}
 			return nil
 		})
-		templ_7745c5c3_Err = layout.Base("Case Study: SaaS Dashboards", csrfToken).Render(templ.WithChildren(ctx, templ_7745c5c3_Var2), templ_7745c5c3_Buffer)
+		templ_7745c5c3_Err = layout.Base("Case Study: SaaS Dashboards", csrfToken, "/cases").Render(templ.WithChildren(ctx, templ_7745c5c3_Var2), templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
