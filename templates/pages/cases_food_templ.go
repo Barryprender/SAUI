9f672ea10e60d4cd9d0751a2d7b94b25fc8e276d
BarryPRE_78 <barryprendergast78@gmail.com>
2026-05-13 16:34:02 +0200

    Regenerate cases_food page

--- templates/pages/cases_food_templ.go
+++ templates/pages/cases_food_templ.go
@@ -289,7 +289,7 @@ func CaseFoodOrdering(csrfToken string) templ.Component {
 			}
 			return nil
 		})
-		templ_7745c5c3_Err = layout.Base("Case Study: Food Ordering", csrfToken).Render(templ.WithChildren(ctx, templ_7745c5c3_Var2), templ_7745c5c3_Buffer)
+		templ_7745c5c3_Err = layout.Base("Case Study: Food Ordering", csrfToken, "/cases").Render(templ.WithChildren(ctx, templ_7745c5c3_Var2), templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
