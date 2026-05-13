b078a9fe5e2c3d9acd3d1d3df3936384cb703559
BarryPRE_78 <barryprendergast78@gmail.com>
2026-05-13 16:34:01 +0200

    Regenerate cases page

--- templates/pages/cases_templ.go
+++ templates/pages/cases_templ.go
@@ -141,7 +141,7 @@ func Cases(csrfToken string) templ.Component {
 			}
 			return nil
 		})
-		templ_7745c5c3_Err = layout.Base("Case Studies", csrfToken).Render(templ.WithChildren(ctx, templ_7745c5c3_Var2), templ_7745c5c3_Buffer)
+		templ_7745c5c3_Err = layout.Base("Case Studies", csrfToken, "/cases").Render(templ.WithChildren(ctx, templ_7745c5c3_Var2), templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
