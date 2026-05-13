01f94c3ae132bd6ed38e54abec738bbd7f64a29b
BarryPRE_78 <barryprendergast78@gmail.com>
2026-05-13 16:34:02 +0200

    Regenerate cases_mfe page

--- templates/pages/cases_mfe_templ.go
+++ templates/pages/cases_mfe_templ.go
@@ -224,7 +224,7 @@ func CaseMFE(csrfToken string) templ.Component {
 			}
 			return nil
 		})
-		templ_7745c5c3_Err = layout.Base("Case Study: Micro Frontends", csrfToken).Render(templ.WithChildren(ctx, templ_7745c5c3_Var2), templ_7745c5c3_Buffer)
+		templ_7745c5c3_Err = layout.Base("Case Study: Micro Frontends", csrfToken, "/cases").Render(templ.WithChildren(ctx, templ_7745c5c3_Var2), templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
