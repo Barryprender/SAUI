dbbe5b3332a0119db0b3929703a05bbe6c258437
BarryPRE_78 <barryprendergast78@gmail.com>
2026-06-02 16:44:38 +0200

    Wire healthcare and saas migrations into store init

--- statestore/store.go
+++ statestore/store.go
@@ -40,6 +40,14 @@ func New(path string, logger *slog.Logger) (*Store, error) {
 		db.Close()
 		return nil, err
 	}
+	if err := s.migrateHealthcareDemo(); err != nil {
+		db.Close()
+		return nil, err
+	}
+	if err := s.migrateSaasDemo(); err != nil {
+		db.Close()
+		return nil, err
+	}
 	return s, nil
 }
 
